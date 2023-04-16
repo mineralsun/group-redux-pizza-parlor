21bd7d72da6b7fdf872229d96d24011aeb8805fa
Everett Butler <everettjamesbutler@gmail.com>
2023-04-16 18:37:29 -0500

    got router/navigation set up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,54 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import PizzaList from '../PizzaList/PizzaList.jsx';
 
 function App() {
 
   return (
+
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-      <PizzaList />
+      <Router>
+        <div>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/api/pizza">Pizza List!</Link>
+            </li>
+            <li>
+              <Link to="/api/order">Order Form</Link>
+            </li>
+            <li>
+              <Link to="/checkout">Checkout</Link>
+            </li>
+          </ul>
+          <Route exact path="/">
+            HOME PAGE
+          </Route>
+          <Route exact path="/api/pizza">
+            <PizzaList />
+          </Route>
+          <Route exact path="/api/order">
+            {/* <CustomerForm /> */}
+          </Route>
+          <Route exact path="/checkout">
+            {/* <Checkout /> */}
+          </Route>
+          <Route exact path="/admin">
+            {/* <Admin /> */}
+          </Route>
+        </div>
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
+      </Router>
     </div>
+
   );
 }
 
