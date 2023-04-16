ae77fa508b0f283632f7a08e216836c526228bb1
Seth Woodson <sethwoodson3@gmail.com>
2023-04-16 18:52:35 -0500

    redux-added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+//Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+
+
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger)
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
