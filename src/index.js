52fb525c5a7db1604d038089927a0c8a62841f2d
Everett Butler <everettjamesbutler@gmail.com>
2023-04-23 18:38:50 -0500

    trying to fix merge problems

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
+
 // Reducer for pizzas
 const pizzas = (state = [], action) => {
     if (action.type === 'SET_PIZZAS') {
@@ -56,20 +57,52 @@ const total = (state = 0, action) => {
     }
 };
 
-// //Items in the cart
-// const cart = (state = [], action) => {
-//     if (action.type === 'ADD_ORDER') {
-//         return [...state, action.payload]
-//     }
-//     return state;
-// };
+const customer_name = (state = '', action) => {
+    if (action.type === 'SET_CUSTOMER_NAME') {
+        return (action.payload);
+    }
+    return state;
+}
 
-// Redux store 
+const street_address = (state = '', action) => {
+    if (action.type === 'SET_STREET_ADDRESS') {
+        return (action.payload);
+    }
+    return state;
+}
+
+const city = (state = '', action) => {
+    if (action.type === 'SET_CITY') {
+        return (action.payload);
+    }
+    return state;
+}
+
+const zip = (state = '', action) => {
+    if (action.type === 'SET_ZIP') {
+        return (action.payload);
+    }
+    return state;
+}
+
+const type = (state = 'Pickup' || 'Delivery', action) => {
+    if (action.type === 'SET_TYPE') {
+        return (action.payload);
+    }
+    return state;
+}
+
+
 const storeInstance = createStore(
     combineReducers({
         cart,
-        pizzas,
+        customer_name,
+        street_address,
+        city,
+        zip,
+        type,
         total,
+        pizzas,
     }),
     applyMiddleware(logger)
 );
