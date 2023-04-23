5b0276c55afbd9b967a32c8df86bff961fe12334
Everett Butler <everettjamesbutler@gmail.com>
2023-04-23 16:44:19 -0500

    got basics imported and a fetchPizzaOrder function set up!

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,6 +1,25 @@
 import axios from 'axios'
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function Admin() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const cart = useSelector(store => store.cart);
+
+    useEffect(() => {
+        fetchPizzaOrder();
+    }, []);
+
+    const fetchPizzaOrder = () => {
+        axios.get('/api/orders').then((response) => {
+            dispatch({ type: 'ADD_ORDER', payload: response.data});
+        }).catch((error) => {
+            console.log(`Error in GET: ${error}`);
+            alert('Something went wrong!')
+        });
+    }
 
     return (
         <>
