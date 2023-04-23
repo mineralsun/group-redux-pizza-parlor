555cbff14d4fbac3562de85533167813a1076719
Everett Butler <everettjamesbutler@gmail.com>
2023-04-23 18:40:10 -0500

    fixed a line

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
-
 // Reducer for pizzas
 const pizzas = (state = [], action) => {
     if (action.type === 'SET_PIZZAS') {
