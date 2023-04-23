318549f0f20e0085ecfa2d56c3c78a411621f888
Everett Butler <everettjamesbutler@gmail.com>
2023-04-23 18:36:01 -0500

    hopefully this fixes the problem

--- src/index.js
+++ src/index.js
@@ -8,41 +8,6 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
-const customer_name = (state = '', action) => {
-    if (action.type === 'SET_CUSTOMER_NAME') {
-        return (action.payload);
-    }
-    return state;
-}
-
-const street_address = (state = '', action) => {
-    if (action.type === 'SET_STREET_ADDRESS') {
-        return (action.payload);
-    }
-    return state;
-}
-
-const city = (state = '', action) => {
-    if (action.type === 'SET_CITY') {
-        return (action.payload);
-    }
-    return state;
-}
-
-const zip = (state = '', action) => {
-    if (action.type === 'SET_ZIP') {
-        return (action.payload);
-    }
-    return state;
-}
-
-const type = (state = 'Pickup' || 'Delivery', action) => {
-    if (action.type === 'SET_TYPE') {
-        return (action.payload);
-    }
-    return state;
-}
-
 // Reducer for pizzas
 const pizzas = (state = [], action) => {
     if (action.type === 'SET_PIZZAS') {
@@ -90,6 +55,42 @@ const total = (state = 0, action) => {
             return state;
     }
 };
+
+const customer_name = (state = '', action) => {
+    if (action.type === 'SET_CUSTOMER_NAME') {
+        return (action.payload);
+    }
+    return state;
+}
+
+const street_address = (state = '', action) => {
+    if (action.type === 'SET_STREET_ADDRESS') {
+        return (action.payload);
+    }
+    return state;
+}
+
+const city = (state = '', action) => {
+    if (action.type === 'SET_CITY') {
+        return (action.payload);
+    }
+    return state;
+}
+
+const zip = (state = '', action) => {
+    if (action.type === 'SET_ZIP') {
+        return (action.payload);
+    }
+    return state;
+}
+
+const type = (state = 'Pickup' || 'Delivery', action) => {
+    if (action.type === 'SET_TYPE') {
+        return (action.payload);
+    }
+    return state;
+}
+
 
 const storeInstance = createStore(
     combineReducers({
